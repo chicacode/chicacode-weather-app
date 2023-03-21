ba0767b416fc6bbaed2e87d6d802123b968ff6de
Geraldine <gerahernan.al@gmail.com>
2023-03-20 18:14:11 -0700

    feat: first component weather app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+import WeatherApp from "./components/WeatherApp"
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <WeatherApp />
     </div>
   )
 }
