1dcba5187d36e572882dce626e2e892e2f744bd6
Geraldine <gerahernan.al@gmail.com>
2023-03-25 14:20:58 -0700

    fix: font weight

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -13,7 +13,7 @@ const Search = () => {
             <input
               type="text"
               placeholder="Enter city name"
-              className="px-4 py-3"
+              className="px-4 py-3 font-normal"
               id="city"
               name="city"
             //   onChange={setData}
